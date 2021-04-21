fe1b3747c1d9536f0027a1eddc67edf80ecd15c2
Keith Wong Jun Hsien <keith.wong@hyperganic.com>
2021-04-21 20:03:38 +0800

    Friends scaffold

--- friendsapp/config/routes.rb
+++ friendsapp/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :friends
+  root "friends#index"
+  get "friends/new"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- friendsapp/test/system/friends_test.rb
+++ friendsapp/test/system/friends_test.rb
@@ -18,7 +18,6 @@ class FriendsTest < ApplicationSystemTestCase
     fill_in "First name", with: @friend.first_name
     fill_in "Last name", with: @friend.last_name
     fill_in "Phone", with: @friend.phone
-    fill_in "String", with: @friend.string
     fill_in "Twitter", with: @friend.twitter
     click_on "Create Friend"
 
@@ -34,7 +33,6 @@ class FriendsTest < ApplicationSystemTestCase
     fill_in "First name", with: @friend.first_name
     fill_in "Last name", with: @friend.last_name
     fill_in "Phone", with: @friend.phone
-    fill_in "String", with: @friend.string
     fill_in "Twitter", with: @friend.twitter
     click_on "Update Friend"
 
