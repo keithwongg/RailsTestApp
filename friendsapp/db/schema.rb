84506911f42d6723e17ec79a16bdf1ab3d9d76eb
Keith Wong Jun Hsien <keith.wong@hyperganic.com>
2021-04-22 10:22:31 +0800

    Validation and tests -- User Model

--- friendsapp/db/schema.rb
+++ friendsapp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_21_111338) do
+ActiveRecord::Schema.define(version: 2021_04_22_021954) do
 
   create_table "friends", force: :cascade do |t|
     t.string "first_name"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 2021_04_21_111338) do
     t.string "twitter"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
