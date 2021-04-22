5cddddfbb7b250354f6e13d3b5dc9b3e06893535
Keith Wong Jun Hsien <keith.wong@hyperganic.com>
2021-04-22 08:49:18 +0800

    Added pokemon sprites and User controller

--- friendsapp/config/routes.rb
+++ friendsapp/config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  resources :friends
-  root "friends#index"
-  get "friends/new"
+  get 'users/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "home#index"
 end
