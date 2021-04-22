f58b7ca508ef0d38a5bb17daf3ddf97e03ded245
Keith Wong Jun Hsien <keith.wong@hyperganic.com>
2021-04-22 10:45:55 +0800

    Passwords and Hashing

--- friendsapp/db/schema.rb
+++ friendsapp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_22_021954) do
+ActiveRecord::Schema.define(version: 2021_04_22_022928) do
 
   create_table "friends", force: :cascade do |t|
     t.string "first_name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2021_04_22_021954) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
